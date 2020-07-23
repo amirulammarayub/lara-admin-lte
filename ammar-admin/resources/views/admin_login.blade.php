1247789a25baffb3c8dd07aed2360a960c6e61cd
amirulammarayub <ammarayub2112@gmail.com>
2020-07-24 07:42:04 +0800

    update view

--- ammar-admin/resources/views/admin_login.blade.php
+++ ammar-admin/resources/views/admin_login.blade.php
@@ -8,15 +8,15 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../../vendor/almasaeed2010/adminlte/bower_components/font-awesome/css/font-awesome.min.css">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/bower_components/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="../../../vendor/almasaeed2010/adminlte/bower_components/Ionicons/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="../../../vendor/almasaeed2010/adminlte/dist/css/AdminLTE.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/plugins/iCheck/square/blue.css">
+    <link rel="stylesheet" href="../../../vendor/almasaeed2010/adminlte/plugins/iCheck/square/blue.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -66,11 +66,11 @@
     <!-- /.login-box -->
 
     <!-- jQuery 3 -->
-    <script src="../../vendor/almasaeed2010/adminltebower_components/jquery/dist/jquery.min.js"></script>
+    <script src="../../../vendor/almasaeed2010/adminltebower_components/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap 3.3.7 -->
-    <script src="../../vendor/almasaeed2010/adminltebower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../../../vendor/almasaeed2010/adminltebower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- iCheck -->
-    <script src="../../vendor/almasaeed2010/adminlteplugins/iCheck/icheck.min.js"></script>
+    <script src="../../../vendor/almasaeed2010/adminlteplugins/iCheck/icheck.min.js"></script>
     <script>
         $(function() {
             $('input').iCheck({
